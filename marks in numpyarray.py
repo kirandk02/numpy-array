150a5ed0463f2bdb92de084d84425be9ec6c3941
kirandk02 <70264092+kirandk02@users.noreply.github.com>
2020-09-21 01:58:02 -0700

    Add files via upload

---
+++ marks in numpyarray.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 13:43:32 2020
+
+@author: KIRAN HP
+"""
+
+import numpy as np
+my_array=np.genfromtxt('C:/Users/HP/Desktop/numpy_redtext.txt')
+print(my_array)
